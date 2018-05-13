6420f0560c0a5c27d28d219c8154c945e6195674
Aki <aki@kaysubs.de>
2018-05-14 00:38:14 +0200

    Configure SnapshotExample to use serize

--- src/main/scala/sample/persistence/SnapshotExample.scala
+++ src/main/scala/sample/persistence/SnapshotExample.scala
@@ -1,10 +1,15 @@
 package sample.persistence
 
+import serize._
 import akka.actor._
 import akka.persistence._
 
 object SnapshotExample extends App {
-  final case class ExampleState(received: List[String] = Nil) {
+  @serize("Snapshot.Message")
+  case class Message(@id(0)evt: String)
+
+  @serize("Snapshot.State")
+  final case class ExampleState(@id(0)received: List[String] = Nil) {
     def updated(s: String): ExampleState = copy(s :: received)
     override def toString = received.reverse.toString
   }
@@ -20,14 +25,14 @@ object SnapshotExample extends App {
       case SaveSnapshotSuccess(metadata)         => // ...
       case SaveSnapshotFailure(metadata, reason) => // ...
       case s: String =>
-        persist(s) { evt => state = state.updated(evt) }
+        persist(Message(s)) { msg => state = state.updated(msg.evt) }
     }
 
     def receiveRecover: Receive = {
       case SnapshotOffer(_, s: ExampleState) =>
         println("offered state = " + s)
         state = s
-      case evt: String =>
+      case Message(evt) =>
         state = state.updated(evt)
     }
 
